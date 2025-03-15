d68a029c8467eb9572c62dac5fa50983fea843d7
youyu-shuaiguo <3320298100@qq.com>
2025-03-15 16:02:27 +0800

    Update wangye.py

--- wangye.py
+++ wangye.py
@@ -7,7 +7,7 @@ with st.sidebar:
         st.write(f'原来已经{ages}岁了....')
     sure = st.button("点击here")
     if sure:
-        st.write('你已确定')
+        st.write('❤王亲晴'))
 st.write('欢迎来到洛阳🥻')
 date = [{"lat": 34.605, "lon": 112.415}]
 st.map(date, zoom = 12)
